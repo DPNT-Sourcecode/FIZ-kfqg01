2fdf2e1b831c946a866402233d89fb253c3f5e8a
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:26:15 +0000

    Mon Apr 08 10:26:15 UTC 2019

---
+++ test/HLO/hello_test.rb
@@ -0,0 +1,14 @@
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'HLO'
+
+class ClientTest < Minitest::Test
+
+  def test_sum
+    assert_equal "soroush", Hello.new.hello("soroush"), 'App should return a string'
+  end
+
+end
