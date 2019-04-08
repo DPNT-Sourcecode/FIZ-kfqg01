4b69e3396e71fbbf0d35cc6a218a1eec9e0260f6
sbx_user1051 <sbx_user1051@169.254.12.181>
2019-04-08 13:30:16 +0000

    Mon Apr 08 13:30:16 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,9 +2,13 @@
 class FizzBuzz
 
   def fizz_buzz(number)
+    return "fizz buzz fake deluxe" if is_deluxe?(number) && (is_fizz?(number) && is_buzz?(number)) && is_fake?(number)
     return "fizz buzz deluxe" if is_deluxe?(number) && (is_fizz?(number) && is_buzz?(number))
+    return "fizz fake deluxe" if is_deluxe?(number) && is_fizz?(number) && is_fake?(number)
     return "fizz deluxe" if is_deluxe?(number) && is_fizz?(number)
+    return "buzz fake deluxe" if is_deluxe?(number) && is_buzz?(number) && is_fake?(number)
     return "buzz deluxe" if is_deluxe?(number) && is_buzz?(number)
+    return "fake deluxe" if is_deluxe?(number) && is_fake?(number)
     return "deluxe" if is_deluxe?(number)
     return "fizz buzz" if is_fizz?(number) && is_buzz?(number)
     return "buzz" if is_buzz?(number)
@@ -25,4 +29,9 @@ class FizzBuzz
   def is_deluxe?(number)
     number.to_s.chars.uniq.length == 1 && number > 10
   end
+
+  def is_fake?(number)
+    return number%2 == 1
+  end
 end
+
