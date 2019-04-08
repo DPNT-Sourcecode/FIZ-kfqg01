77d393ee5c287e2d1e9e000414794432270b396b
sbx_user1051 <sbx_user1051@169.254.12.181>
2019-04-08 13:01:28 +0000

    Mon Apr 08 13:01:28 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,15 +2,28 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    return "fizz buzz deluxe" if (number.to_s.chars.uniq.length == 1 && number > 10) && ((number%5 == 0) || (number.to_s.chars.include?("5")) && ((number%3 ==0) || number.to_s.chars.include?("3")))
-    return "deluxe" if (number.to_s.chars.uniq.length == 1 && number > 10)
-    return "fizz buzz" if (((number%5 == 0) || (number.to_s.chars.include?("5"))) && ((number%3 ==0) || number.to_s.chars.include?("3")))
+    return "fizz buzz deluxe" if is_deluxe?(number) && (is_fizz?(number) && is_buzz?(number))
+    return "deluxe" if is_deluxe?(number)
+    return "fizz buzz" if is_fizz?(number) && is_buz?(number)
     return "buzz" if number % 5 == 0 || number.to_s.chars.include?("5")
     return "fizz"  if number % 3 ==0 || number.to_s.chars.include?("3")
     return number
   end
 
+  private
+  def is_fizz?(number)
+    return number % 3 ==0 || number.to_s.chars.include?("3")
+  end
+
+  def is_buzz?(number)
+    return number%5 == 0 || number.to_s.chars.include?("5")
+  end
+
+  def is_deluxe?(number)
+    number.to_s.chars.uniq.length == 1 && number > 10
+  end
 end
+
 
 
 
