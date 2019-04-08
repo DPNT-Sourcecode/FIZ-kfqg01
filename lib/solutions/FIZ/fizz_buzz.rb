725993a66d1f27e6ae948c2339c5e03ad41c2669
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 11:41:47 +0000

    Mon Apr 08 11:41:47 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,9 +2,8 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    if (number % 3 == 0 || number.to_s.chars.include?("3")) && (number % 5 ==0 || number.to_s.chars.include?("5"))
+    if ((number % 3 == 0 || number.to_s.chars.include?("3")) && (number % 5 ==0 || number.to_s.chars.include?("5")))
       return "fizz buzz"
-
     elsif number % 5 == 0 || number.to_s.chars.include?("5")
       return "buzz"
     else
@@ -13,5 +12,6 @@ class FizzBuzz
   end
 
 end
+
 
 
