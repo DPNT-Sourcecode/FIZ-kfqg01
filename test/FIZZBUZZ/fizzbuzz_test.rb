4673c1e10f3f171aee4f96c37b61ced9a20b243a
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:53:21 +0000

    Mon Apr 08 10:53:21 UTC 2019

--- test/FIZZBUZZ/fizzbuzz_test.rb
+++ test/FIZZBUZZ/fizzbuzz_test.rb
@@ -8,7 +8,9 @@ require_solution 'FIZ'
 class ClientTest < Minitest::Test
 
   def test_fizzbuzz
-    assert_equal "fizz", FizzBuzz.new.fizzbuzz(3), 'App should return fizz for multiple of 3'
+    assert_equal "fizz", FizzBuzz.new.fizz_buzz(3), 'App should return fizz for multiple of 3'
+    assert_equal "buzz", FizzBuzz.new.fizz_buzz(5), 'App should return buzz for multiple of 5'
   end
 
 end
+
